279be604dcdac3a7152588169168d03512ec9f14
nexusDev <nexotech.dev@gmail.com>
2026-03-26 18:11:01 +0000

    lastCommit

--- traitementAjout.php
+++ traitementAjout.php
@@ -1,2 +1,13 @@
 <?php
-if(isset($POST["ajouter"]))
+if(isset($POST["ajouter"])){
+    $newPlat =[
+        "code" =>count($plats)+1,
+        "nom" => $_POST["nom"],
+        "prix"=> $_POST["prix"],
+        "categorie"=> $_POST["categorie"],
+        "disponibilite"=> $_POST["disponibilite"],
+    ];
+    addNewPlat($plats,$newPlat);
+    echo"Plat Ajouté";
+   header("Location : index.php?page=liste")
+}
